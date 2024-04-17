c1395d08628a4b092b581d10d8cc085711c1b2df
Ali11-tech's <zainzawar72@gmail.com>
2024-04-17 22:27:01 +0500

    Add method to create array

--- MyList.py
+++ MyList.py
@@ -6,8 +6,12 @@ class MyList:
         self.n = 0 
         self.A = self.__make_array(self.size)
 
+    def __make_array(self,capacity):
+        return (capacity*ctypes.py_object)()
+
     def __len__(self):
         return self.n
+
 
 
 x = MyList()
