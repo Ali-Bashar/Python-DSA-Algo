94bdff4881feda9661425c897096514ecdc81b39
Ali11-tech's <zainzawar72@gmail.com>
2024-04-17 21:34:58 +0500

    Add sum_odd_nodes

--- Linked_List.py
+++ Linked_List.py
@@ -149,4 +149,19 @@ class linked_list:
 
         maxi.data = value
 
-    
+    def sum_odd_nodes(self):
+
+        temp = self.head
+        counter = 0
+        result = 0
+
+        while temp != None:
+            if counter % 2 != 0:
+                result = result + temp.data
+
+            counter += 1
+            temp = temp.next
+
+        print(result)
+
+
