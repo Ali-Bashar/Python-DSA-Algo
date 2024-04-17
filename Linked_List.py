72ed3053afb67cebfeb2cdcfff5a74454162d407
Ali11-tech's <zainzawar72@gmail.com>
2024-04-17 21:59:10 +0500

    Add revers method

--- Linked_List.py
+++ Linked_List.py
@@ -165,3 +165,19 @@ class linked_list:
         print(result)
 
 
+    def revers(self):
+
+        prev_node = None
+        curr_node = self.head
+
+        while curr_node != None:
+            next_node = curr_node.next
+            curr_node.next =  prev_node
+            prev_node = curr_node
+            curr_node = next_node
+
+        self.head = prev_node
+
+
+
+
