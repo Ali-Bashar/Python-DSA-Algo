e4bd8ebdb6b2db7008a1bd9c0ecfe9ca4b5a86e0
Ali11-tech's <zainzawar72@gmail.com>
2024-04-17 22:24:09 +0500

    Add method

--- MyList.py
+++ MyList.py
@@ -5,4 +5,10 @@ class MyList:
         self.size = 1
         self.n = 0 
         self.A = self.__make_array(self.size)
-        
+
+    def __len__(self):
+        return self.n
+
+
+x = MyList()
+print(len(x))
