c9f8be34b0fe145ce25c3e246abbbb5d6b3feab6
Ali11-tech's <zainzawar72@gmail.com>
2024-04-18 21:01:49 +0500

    Modified Index Method

--- MyList.py
+++ MyList.py
@@ -41,8 +41,8 @@ class MyList:
     def __getitem__(self,index):
         if 0<= index < self.n:
             return self.A[index]
-        else:
-            return "IndexError"
+        
+        raise IndexError("Item Not Found")
 
 
 
